fc069a64cb94c142b00127e39e389b601ea791f2
WinnerWind <contact@winnerwind.in>
2025-09-13 20:26:41 +0530

    Basic docs panel (no closing)

--- scripts/ui/views/help_panel.gd
+++ scripts/ui/views/help_panel.gd
@@ -6,47 +6,47 @@ class_name HelpPanel
 @export_dir var docs_path:String
 
 func _ready() -> void:
-	load_docs_in_dir(docs_path)
+	get_directories_recursive(docs_path)
 
+func get_directories_recursive(path:String) -> void:
+	var dir := DirAccess.open(path)
+	if dir == null:
+		printerr("Could not open: ", path)
+		return
+	var subdirs := dir.get_directories()
+	if subdirs.is_empty(): return
+	for subdir in subdirs:
+		var child_abs := path.rstrip("/") + "/" + subdir
+		var parent_name := child_abs.get_base_dir().trim_prefix(docs_path).split("/")[-1]
+		add_to_tree(subdir,parent_name)
+		get_directories_recursive(child_abs)
 
-func load_docs_in_dir(directory_path:String = "") -> void:
-	var dir_name := directory_path.trim_prefix(directory_path.get_base_dir()+"/")
-	var parent_dir_name := directory_path.trim_suffix("/"+dir_name).trim_prefix(docs_path+"/")
-	var dir = DirAccess.open(directory_path)
-	set_tree_button(dir_name, parent_dir_name if not parent_dir_name == docs_path.trim_suffix("/"+dir_name) else "")
-	for directory:String in dir.get_directories():
-		load_docs_in_dir(directory_path +"/" + directory)
-
-func load_index_file(path:String) -> void:
-	var index_path := path + "/index.md"
-	if FileAccess.file_exists(index_path):
-		var file = FileAccess.open(index_path, FileAccess.READ)
-
-func set_tree_button(child_name:String, parent_name:String = "") -> void:
-	if parent_name == "": #looking for root
+func add_to_tree(child_name:String, parent_name:String = "") -> void:
+	if parent_name == "": # root element
 		var parent = tree.create_item()
 		parent.set_text(0,child_name)
-	else: #Has a parent
-		# find parent item
-		print(find_tree_item_by_text(tree.get_root(), parent_name).get_text(0))
-		var parent = find_tree_item_by_text(tree.get_root(), parent_name)
+	else:
+		var parent = find_tree_item_by_name(parent_name)
 		var child = tree.create_item(parent)
-		child.set_text(0,child_name)
+		child.set_text(0, child_name)
 
-func find_tree_item_by_text(current_item: TreeItem, target_text: String, column_index: int = 0) -> TreeItem:
-	if current_item == null:
-		return null
+func find_tree_item_by_name(child_name:String) -> TreeItem:
+	var child = tree.get_root() #start from root
+	while child.get_text(0) != child_name:
+		child = child.get_next_in_tree()
+	return child
 
-	# Check the text of the current item in the specified column
-	if current_item.get_text(column_index) == target_text:
-		return current_item
+func _on_tree_cell_selected() -> void:
+	var item := tree.get_selected()
+	var path:String
+	while item != null: #Iterate over the tree and get path
+		path = item.get_text(0)+"/"+path
+		item = item.get_parent()
+	path = docs_path +"/"+ path #get docs path bcak
+	set_text_content(path)
 
-	# Check children
-	var child = current_item.get_first_child()
-	while child != null:
-		var found_item = find_tree_item_by_text(child, target_text, column_index)
-		if found_item != null:
-			return found_item
-		child = child.get_next()
-
-	return null
+func set_text_content(absolute_path:String) -> void:
+	var file = FileAccess.open(absolute_path+"index.md", FileAccess.READ)
+	if not file: return
+	
+	content_label.text = file.get_as_text()
